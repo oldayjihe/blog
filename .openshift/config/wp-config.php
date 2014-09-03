edd7a8dc051021b21dee44c8edf1ac540ce7b1e4
westminboy <westminboy@gmail.com>
2014-09-03 11:52:50 +0800

    fix cockies block

--- .openshift/config/wp-config.php
+++ .openshift/config/wp-config.php
@@ -108,8 +108,9 @@ $table_prefix  = 'wp_';
  * language support.
  */
 define('WPLANG', 'zh_CN');
-define('WP_POST_REVISIONS', false);//禁用历史修订版本
-define('AUTOSAVE_INTERVAL', 86400);//设置自动保存时间设置为一天
+define('WP_POST_REVISIONS', false);
+define('AUTOSAVE_INTERVAL', 86400);
+
 /**
  * For developers: WordPress debugging mode.
  *
